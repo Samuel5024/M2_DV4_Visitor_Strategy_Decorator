ccdabc73b538aaeef6aa2932393f2586c6105a23
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-23 11:45:33 -0700

    Add IWeapon.cs & WeaponDecorator.cs

---
+++ Visitor Strategy Decorator/Assets/Scripts/IWeapon.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class IWeapon
+{
+    float Range
+    {
+        get;
+    }
+
+    float Duration
+    {
+        get;
+    }
+
+    float Strength
+    {
+        get;
+    }
+
+    float Cooldown
+    {
+        get;
+    }
+}
