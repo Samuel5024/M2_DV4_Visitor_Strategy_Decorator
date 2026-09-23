e0a8c46000495f622feff63b7c1bb1e5e9a95f6e
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-22 22:31:41 -0700

    Add ClientVisitor.cs

---
+++ Visitor Strategy Decorator/Assets/Scripts/ClientVisitor.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ClientVisitor : MonoBehaviour
+{
+    public PowerUp enginePowerUp;
+    public PowerUp shieldPowerUp;
+    public PowerUp weaponPowerUp;
+
+    private BikeController _bikeController;
+    
+    void Start()
+    {
+        _bikeController = gameObject.AddComponent<BikeController>();
+    }
+
+    void OnGUI()
+    {
+        if(GUILayout.Button("PowerUp Shiled"))
+        {
+            _bikeController.Accept(shieldPowerUp);
+        }
+        if(GUILayout.Button("PowerUp Engine"))
+        {
+            _bikeController.Accept(enginePowerUp);
+        }
+        if(GUILayout.Button("PowerUp Weapon"))
+        {
+            _bikeController.Accept(weaponPowerUp);
+        }
+    }
+}
